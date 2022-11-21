0af28c5445e0d7a6f13bcb530d901f19debe6e87
RE <jogger-heirs-0e@icloud.com>
2022-11-21 17:39:50 +0000

    Change harness to read file from stdin.
    Harness performs normally on own, but AFL stuck on one path

--- test_src/test.c
+++ test_src/test.c
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/stat.h>
 
@@ -15,42 +16,44 @@
 
 int main(int argc, char **argv) {
   int upng_test_res, lodepng_test_res;
-  int log_count = 0;
   char* log_file = "log.txt";
-  char* latest_poc = "poc";
-  int chr;
-  FILE* poc;
-  FILE* input;
   FILE* log;
+  char temp_path[] = "/tmp/fuzz_XXXXXX";
+  FILE* temp_file;
+  int temp_fd;
+  char buf;
   if(DEBUG)puts("DEBUG: running main");
-  if(argc < 2){
-        fprintf(stderr, "Usage: %s <png>\n", argv[0]);
-        return EXIT_FAILURE;
+  if(DEBUG)puts("DEBUG: reading stdin to temp file");
+  temp_fd = mkstemp(temp_path);
+  if(temp_fd < 0){
+    if(DEBUG)puts("DEBUG: error creating temp file");
+    return 0;
   }
-  
+  close(temp_fd);
+  temp_file = fopen(temp_path, "w+b");
+  if(!temp_file){
+    if(DEBUG)puts("DEBUG: error opening temp file");
+    unlink(temp_path);
+    return 0;
+  }
+  while(fread(&buf, 1, 1, stdin) > 0){
+      fwrite(&buf, 1, 1, temp_file);
+  }
+  fclose(temp_file);
+  if(DEBUG)printf("DEBUG: copied input to temp file %s\n", temp_path);
   if(DEBUG)printf("DEBUG: executing upng decoder\n\n");
-  upng_test_res = upng_decoder_test(argv[1]);
+  upng_test_res = upng_decoder_test(temp_path);
  
   /* we get the result from the upng decoder. 0 == success; otherwise, failure. */
   if(DEBUG)printf("\nDEBUG: back in main. upng decoder result: %i\n\n", upng_test_res); 
   
   if(DEBUG)puts("DEBUG: executing lodepng decoder");
-  lodepng_test_res = lodepng_decoder_test(argv[1]);
+  lodepng_test_res = lodepng_decoder_test(temp_path);
   
   log = fopen(log_file, "a");
   /* Differential fuzzing*/
-  if((upng_test_res == UPNG_EOK) != (lodepng_test_res == 0) && strcmp(argv[1], latest_poc)){
+  if((upng_test_res == UPNG_EOK) != (lodepng_test_res == 0)){
     if(DEBUG)puts("DEBUG: program outputs differ");
-    
-    poc = fopen(latest_poc, "w");
-    input = fopen(argv[1], "r");
-    if(poc && input){
-      while((chr = fgetc(input)) != EOF){
-        fputc(chr, poc);
-      }
-      fclose(poc);
-      fclose(input);
-    }
     if(log){
       fprintf(log, "u:%d lode:%d\n", upng_test_res, lodepng_test_res);
     }
@@ -59,6 +62,9 @@ int main(int argc, char **argv) {
   }
   if(log){
     fclose(log);
+  }
+  if(unlink(temp_path) < 0){
+    if(DEBUG)printf("DEBUG: error deleting temp file %s\n", temp_path);
   }
   if(DEBUG)printf("\nDEBUG: back in main. lodepng decoder result: %i\n", lodepng_test_res);
   if(!DEBUG)printf("INFO: decoding has been completed. upng returned %i and lodepng returned %i.\n", upng_test_res, lodepng_test_res);
